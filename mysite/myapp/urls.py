d0ffd1cd53016fabbf15a88d0acc81392724bc0a
Pheerawat Kongsap <pheerawat.ko.64@ubu.ac.th>
2023-12-11 18:17:56 +0700

    'A'

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -19,5 +19,4 @@ from myapp import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('delete/<int:id>/', views.delete, name='delete'),
 ]
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -5,8 +5,3 @@ from myapp.models import Course
 def home(req):
     data = Course.objects.all()
     return render(req, 'myapp/home.html', {'data': data})
-
-def delete(req, id):
-    data = Course.objects.get(pk=id)
-    data.delete()
-    return redirect('/')
