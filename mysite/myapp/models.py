294a238ec9cd478c8fe2149805a5b20185da6c72
Pheerawat Kongsap <pheerawat.ko.64@ubu.ac.th>
2023-12-11 18:16:02 +0700

    'E'

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Course(models.Model):
+    course_id = models.IntegerField()
+    course_name = models.CharField(max_length=50)
+    def __str__(self):
+        return f'{self.course_id} : {self.course_name}'
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from myapp.models import Course
 
 # Create your views here.
+def home(req):
+    data = Course.objects.all()
+    return render(req, 'myapp/home.html', {'data': data})
+
+def delete(req, id):
+    data = Course.objects.get(pk=id)
+    data.delete()
+    return redirect('/')
